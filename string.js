ee68d511c02103bca8f17ad2d8825630c0051d55
Younus Ali Akash <younusaliakash8@gmail.com>
2021-03-02 19:35:52 +0600

    different using of string

---
+++ string.js
@@ -0,0 +1,43 @@
+// var str = 'This is  String'
+// console.log(str)
+
+// //    with cortation
+
+// var str = 'This is  \'String\''
+// console.log(str)
+
+// // with new line
+// var str = 'This is  \nString'
+// console.log(str)
+
+// // with tab space
+// var str = 'This is  \tString'
+// console.log(str)
+
+// // with backwordslash
+// var str = 'This is  \\String'
+// console.log(str)
+
+// /// ******* String Corperison*****//
+
+// //////////************************** */
+
+// var a = 'I am'
+// var b = 'Younus Ali Akash'
+
+// var c = a.concat(' ', b)
+
+// console.log(c)
+// console.log(c.substring(5, 10))
+// console.log(c.toUpperCase())
+// console.log(c.toLowerCase())
+// console.log(c.startsWith('I am'))
+// console.log(c.endsWith('h'))
+// console.log(c.charAt(5))
+// console.log(c.split(' '))
+// console.log( '               gsjfakg          '.trim())
+
+///   String Lenth
+var s = 'Akash'
+
+console.log(s.length)
